a8e1c652fb88ae41d65c09827c501ead03dae750
Leena Komenski <lkomenski@gmail.com>
2025-08-07 16:17:24 -0700

    Update test_basics.py

--- .github/test_basics.py
+++ .github/test_basics.py
@@ -1,28 +1,43 @@
-
 import pytest
 
-# Sample functions to test
-def add(a, b):
-    return a + b
+def test_score_increment():
+    snake_pos = [100, 50]
+    food_pos = [100, 50]
+    score = 0
+    food_spawn = True
 
-def subtract(a, b):
-    return a - b
+    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
 
-def is_game_over(score, max_score):
-    return score >= max_score
+    assert score == 1
+    assert food_spawn is False
 
-# Test cases
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+def test_direction_change_prevention():
+    direction = 'UP'
+    change_to = 'DOWN'
 
-def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 4) == -4
-    assert subtract(-2, -2) == 0
+    # Prevent reversal
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    elif change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    elif change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    elif change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
 
-def test_is_game_over():
-    assert is_game_over(10, 10) is True
-    assert is_game_over(9, 10) is False
-    assert is_game_over(15, 10) is True
+    # Should not allow reversal
+    assert direction == 'UP'
+
+def test_out_of_bounds_detection():
+    frame_size_x = 720
+    frame_size_y = 480
+    snake_pos = [730, 50]  # Out of bounds on X
+
+    out_of_bounds = (
+        snake_pos[0] < 0 or snake_pos[0] > frame_size_x - 10 or
+        snake_pos[1] < 0 or snake_pos[1] > frame_size_y - 10
+    )
+
+    assert out_of_bounds is True
